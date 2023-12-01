91a30a7c93e8af71ae236dbd9563573f97112019
Måns Dahlström <mans@mansdahlstrom.se>
2023-12-02 00:48:45 +0100

    [Rust] - 2023 day 1 pt1

--- rust/src/_2023/day1/mod.rs
+++ rust/src/_2023/day1/mod.rs
@@ -2,5 +2,20 @@ use crate::utils;
 
 pub fn main() {
   let input = utils::get_input_file("./src/_2023/day1/input.txt");
-  println!("Day 1: {}", input);
+  let lines = input.split('\n');
+  let mut result: i32 = 0;
+
+  for line in lines {
+    if line.is_empty() {
+      continue;
+    }
+    let numbers: Vec<char> = line.chars().filter(|c| c.is_ascii_digit()).collect();
+    let s: String = numbers.iter().collect();
+    let digits = format!("{}{}", numbers.first().unwrap(), numbers.last().unwrap());
+    let value = digits.parse::<i32>().unwrap();
+    result += value;
+    println!("{} => {} => {}", line, s, digits);
+  }
+
+  println!("Result: {}", result)
 }
