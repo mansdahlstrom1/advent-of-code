f290f7d5799890126e9aa2a7fac9f2bd1da61657
Måns Dahlström <mans@mansdahlstrom.se>
2024-12-02 17:46:52 +0100

    [Go] 2024 day 2 pt 2 (FINALLY, that took way to long)

--- go/src/_2024/day2/day2.go
+++ go/src/_2024/day2/day2.go
@@ -11,12 +11,15 @@ import (
 func Day2() {
 	fmt.Println("Day 2 Solution")
 
-	var files = []string{"example", "input"}
+	var files = []string{"example.txt", "input.txt"}
 	for _, filename := range files {
 		reports := parseInput(filename)
 
-		numberOfSafeReports := part1(reports)
-		fmt.Printf("Number of safe reports for file %s: %d\n", filename, numberOfSafeReports)
+		numberOfSafeReportsPt1 := part1(reports)
+		fmt.Printf("Part 1: Number of safe reports for file %s: %d\n", filename, numberOfSafeReportsPt1)
+
+		numberOfSafeReportsPt2 := part2(reports)
+		fmt.Printf("Part 2: Number of safe reports for file %s: %d\n", filename, numberOfSafeReportsPt2)
 	}
 }
 
@@ -24,24 +27,7 @@ func part1(reports [][]int) int {
 	var numberOfSafeReports int = 0
 
 	for _, report := range reports {
-		fmt.Println(report)
-
-		var prevLevel int = report[0]
-		var isIncreasing bool = report[1] > report[0]
-		var isSafe bool = true
-		for i, level := range report {
-			if i == 0 {
-				continue
-			}
-
-			isSafe = checkSafety(prevLevel, level, isIncreasing)
-			if !isSafe {
-				break
-			}
-
-			prevLevel = level
-		}
-
+		isSafe := checkReportSafety(report, 0)
 		if isSafe {
 			numberOfSafeReports++
 		}
@@ -49,22 +35,89 @@ func part1(reports [][]int) int {
 	return numberOfSafeReports
 }
 
-func checkSafety(prevLevel, level int, isIncreasing bool) bool {
-	if utils.Abs(prevLevel-level) > 3 {
-		return false
-	}
+func part2(reports [][]int) int {
+	var numberOfSafeReports int = 0
 
-	if isIncreasing {
-		if level <= prevLevel {
-			return false
-		}
-	} else {
-		if level >= prevLevel {
-			return false
+	for _, report := range reports {
+		isSafe := checkReportSafety(report, 1)
+		if isSafe {
+			numberOfSafeReports++
 		}
 	}
 
-	return true
+	return numberOfSafeReports
+}
+
+func getIsIncreasing(report []int) bool {
+	var increaseCount int = 0
+	var decreaseCount int = 0
+
+	for i, level := range report {
+		if i+1 == len(report) {
+			break
+		}
+
+		var nextLevel int = report[i+1]
+		if nextLevel > level {
+			increaseCount++
+		} else if nextLevel < level {
+			decreaseCount++
+		}
+	}
+
+	return increaseCount >= decreaseCount
+}
+
+func checkReportSafety(report []int, retryLimit int) bool {
+	fmt.Println("Checking report safety ", report)
+	var isIncreasing bool = getIsIncreasing(report)
+	var retries int = 0
+	isSafe, levelIndex := checkLevelSafety(report, isIncreasing)
+
+	if isSafe {
+		return isSafe
+	}
+
+	// If not retries are allowed, return false
+	if retries+1 > retryLimit {
+		fmt.Println("Check failed... No retries left... returning false")
+		return isSafe
+	}
+
+	// if we have reties left, try to remove the level that caused the issue
+	// Check both possible version of the error
+	fmt.Println("Check failed... retrying,", retries, "of", retryLimit)
+	updatedReport1 := utils.DeleteElementAtIndex(report, levelIndex)
+	updatedReport2 := utils.DeleteElementAtIndex(report, levelIndex+1)
+
+	array1Result := checkReportSafety(updatedReport1, retryLimit-1)
+	array2Result := checkReportSafety(updatedReport2, retryLimit-1)
+
+	// If any of the arrays are safe, return true
+	return array1Result || array2Result
+}
+
+func checkLevelSafety(report []int, isIncreasing bool) (bool, int) {
+	var isSafe bool = false
+	for i, level := range report {
+		if i+1 == len(report) {
+			break
+		}
+
+		var nextLevel int = report[i+1]
+		if isIncreasing {
+			val := nextLevel - level
+			isSafe = val > 0 && val <= 3
+		} else {
+			val := level - nextLevel
+			isSafe = val > 0 && val <= 3
+		}
+
+		if !isSafe {
+			return isSafe, i
+		}
+	}
+	return isSafe, -1
 }
 
 func parseInput(filename string) [][]int {
